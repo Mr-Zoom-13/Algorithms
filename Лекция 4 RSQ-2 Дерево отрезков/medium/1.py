f0cacdd11c65aaa47fbeef348fa6a4d57f079568
MrZoom <mr.zoom.orsk@gmail.com>
2026-08-07 13:30:03 +0300

    lection 4 part 2

--- Лекция 4 RSQ-2 Дерево отрезков/medium/1.py
+++ Лекция 4 RSQ-2 Дерево отрезков/medium/1.py
@@ -5,7 +5,12 @@ import random
 class Node:
     def __init__(self):
         self.max_sum = 0
-        self.max_near_sum = 0
+        self.sum = 0
+        self.pref_sum = 0
+        self.suf_sum = 0
+
+    def __repr__(self):
+        return f"max_sum: {self.max_sum} pref: {self.pref_sum} suf: {self.suf_sum}"
 
 
 
@@ -15,29 +20,48 @@ def set_tree(tree, start, value, l, r, q_l, q_r):
 
     if (r - l) == 1:
         tree[start].max_sum = value
-        tree[start].max_near_sum = value
+        tree[start].sum = value
+        tree[start].pref_sum = value
+        tree[start].suf_sum = value
         return
     m = (l + r) // 2
     set_tree(tree, 2 * start + 1, value, l, m, q_l, q_r)
     set_tree(tree, 2 * start + 2, value, m, r, q_l, q_r)
 
-    tree[start].max_sum = max(tree[2 * start + 1].max_sum, tree[2 * start + 2].max_sum, tree[2 * start + 1].max_near_sum + tree[2 * start + 2].max_near_sum)
 
-    tree[start] = tree[2 * start + 1] + tree[2 * start + 2]
+    tree[start].max_sum = max(tree[2 * start + 1].max_sum, tree[2 * start + 2].max_sum, tree[2 * start + 1].suf_sum + tree[2 * start + 2].pref_sum)
+    tree[start].sum = tree[2 * start + 1].sum + tree[2 * start + 2].sum
+
+    # if (m - l) == 1:
+    #     tree[start].pref_sum = max(tree[2 * start + 1].pref_sum, tree[2 * start + 1].pref_sum + tree[2 * start + 2].pref_sum)
+    # else:
+    #     tree[start].pref_sum = max(tree[2 * start + 1].pref_sum, tree[2 * start + 1].pref_sum + tree[2 * start + 1].suf_sum)
+    #
+    # if (r - m) == 1:
+    #     tree[start].suf_sum = max(tree[2 * start + 2].suf_sum, tree[2 * start + 1].suf_sum + tree[2 * start + 2].pref_sum)
+    # else:
+    #     tree[start].suf_sum = max(tree[2 * start + 2].suf_sum, tree[2 * start + 1].suf_sum + tree[2 * start + 2].pref_sum + tree[2 * start + 2].suf_sum)
 
 
-def get_maximum(tree, start):
-    if tree[start] >= tree[2 * start + 1] and tree[start] >= tree[2 * start + 2]:
-        return tree[start]
-    if tree[start] < tree[2 * start + 1] and tree[2 * start + 1] > tree[2 * start + 2]:
-        return get_maximum(tree, 2 * start + 1)
-    return get_maximum(tree, 2 * start + 2)
+    tree[start].pref_sum = max(tree[2 * start + 1].pref_sum, tree[2 * start + 1].sum + tree[2 * start + 2].pref_sum)
+    tree[start].suf_sum = max(tree[2 * start + 2].suf_sum, tree[2 * start + 2].sum + tree[2 * start + 1].suf_sum)
+
+
+# def get_maximum(tree, start):
+#     if tree[start].max_sum >= tree[2 * start + 1].max_sum and tree[start].max_sum >= tree[2 * start + 2].max_sum:
+#         return tree[start].max_sum
+#     if tree[start].max_sum < tree[2 * start + 1].max_sum and tree[2 * start + 1].max_sum > tree[2 * start + 2].max_sum:
+#         return get_maximum(tree, 2 * start + 1)
+#     return get_maximum(tree, 2 * start + 2)
 
 
 
 def ideal_command(tree, n, l, r, v):
     set_tree(tree, 0, v, 0, n, l, r - 1)
-    return get_maximum(tree, 0)
+    answer = tree[0].max_sum
+    if answer < 0:
+        return 0
+    return answer
 
 
 
@@ -59,7 +83,9 @@ def stupid_command(stupid_lst, n, l, r, v):
 #     for n in range(1, 100):
 #         for m in range(1, 100):
 #             stupid_lst = [0] * n
-#             tree = [0] * (100 * n)
+#             tree = []
+#             for _ in range(4 * n):
+#                 tree.append(Node())
 #             commands = []
 #             for _ in range(m):
 #                 l = random.randint(0, n - 1)
@@ -67,15 +93,27 @@ def stupid_command(stupid_lst, n, l, r, v):
 #                 v = random.randint(-10, 10)
 #                 commands.append(f"{l} {r} {v}")
 #                 stupid_answer = stupid_command(stupid_lst, n, l, r, v)
-#                 ideal_answer = ideal_command(tree, n, l, r, v)
-#                 assert stupid_answer == ideal_answer, f"{n}, {m}, {commands}, {stupid_answer}, {ideal_answer}"
+#                 try:
+#                     ideal_answer = ideal_command(tree, n, l, r, v)
+#                     assert stupid_answer == ideal_answer, f"{n}, {m}, {commands}, {stupid_answer}, {ideal_answer}"
+#                 except Exception as e:
+#                     print(e)
+#                     print(f"{n}, {m}, {commands}, {stupid_answer}")
+#                     exit()
 #
 #
 # generate_samples()
 
+def print_tree(tree):
+    print("-" * 100)
+    for i in range(len(tree)):
+        print(f"{i}. {tree[i]}")
+    print("-" * 100)
 
 n, m = map(int, input().split())
-tree = [Node()] * (4 * n)
+tree = []
+for _ in range(4 * n):
+    tree.append(Node())
 for _ in range(m):
     l, r, v = map(int, input().split())
     print(ideal_command(tree, n, l, r, v))
--- Лекция 4 RSQ-2 Дерево отрезков/medium/2.py
+++ Лекция 4 RSQ-2 Дерево отрезков/medium/2.py
@@ -1,35 +1,56 @@
-# https://codeforces.com/edu/course/2/lesson/5/3/practice/contest/280799/problem/A
+# https://informatics.msk.ru/mod/statements/view.php?id=43201&chapterid=1663#1
 
-n, m = map(int, input().split())
+n, k = map(int, input().split())
+lst = list(map(int, input().split()))
 tree = [0] * (4 * n)
+
+# Собираем дерево
+def build_tree(start, l, r):
+    global tree
+
+    if (r - l) == 1:
+        tree[start] = lst[l]
+        return
+
+    m = (l + r) // 2
+    build_tree(2 * start + 1, l, m)
+    build_tree(2 * start + 2, m, r)
+
+    tree[start] = min(tree[2 * start + 1], tree[2 * start + 2])
 
 
 def set_tree(start, value, l, r, q_l, q_r):
     global tree
 
-    if q_l >= r or q_r < l:
-        return
-
     if (r - l) == 1:
-        tree[start] = value
+        if q_l == l:
+            tree[start] = value
         return
     m = (l + r) // 2
     set_tree(2 * start + 1, value, l, m, q_l, q_r)
     set_tree(2 * start + 2, value, m, r, q_l, q_r)
-    tree[start] = tree[2 * start + 1] + tree[2 * start + 2]
+    tree[start] = min(tree[2 * start + 1], tree[2 * start + 2])
 
 
-def get_maximum(start):
+def get_min(start, l, r, q_l, q_r):
     global tree
 
-    if tree[start] > tree[2 * start + 1] and tree[start] > tree[2 * start + 2]:
+    if q_l >= r or q_r < l:
+        return 10**9
+
+    if (r - l) == 1:
         return tree[start]
-    if tree[start] < tree[2 * start + 1] and tree[2 * start + 1] > tree[2 * start + 2]:
-        return get_maximum(2 * start + 1)
-    return get_maximum(2 * start + 1)
+
+    m = (l + r) // 2
+    return min(get_min(2 * start + 1, l, m, q_l, q_r), get_min(2 * start + 2, m, r, q_l, q_r))
 
 
-for _ in range(m):
-    l, r, v = map(int, input().split())
-    set_tree(0, v, 0, n, l, r - 1)
-    print(get_maximum(0))
+build_tree(0, 0, n)
+
+start = 0
+stop = k - 1
+while stop < n:
+    print(get_min(0, 0, n, start, stop), end=" ")
+    start += 1
+    stop += 1
+
